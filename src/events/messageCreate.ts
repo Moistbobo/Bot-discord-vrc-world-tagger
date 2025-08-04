898af3952c0ccca866e9970332ecc8d8b0ae4649
moistbobo <moistbobo@gmail.com>
2025-08-04 18:54:15 +0800

    docs: add comments

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -11,16 +11,22 @@ import die from './messageCreate/die';
 
 const messageCreate = async (message: Message) => {
   if (message.content.startsWith('.watch')) {
+    // Add a channel to the watch list for tagging.
     return withProtection(watchChannel)(message);
   } else if (message.content.startsWith('.unwatch')) {
+    // Remove a channel from the watch list.
     return withProtection(unWatchChannel)(message);
   } else if (message.content.startsWith('.forwardAndroid')) {
+    // Set a channel as forward target for android compatible worlds. Subsequent calls will overwrite the last.
     return withProtection(androidSupport)(message);
   } else if (message.content.startsWith('.forwardMaxSlots')) {
+    // Set a channel as forward target for worlds with >=60 people. Subsequent calls will overwrite the last.
     return withProtection(maxSlots)(message);
   } else if (message.content.startsWith('.clearForwardingChannels')) {
+    // Clear the forwarding targets (done like this because I am lazy)
     return withProtection(clearForwardingChannels)(message);
   } else if (message.content.startsWith('.die')) {
+    // Kills the bot before it kills us
     return withProtection(die)(message);
   } else {
     return watchForVRCWorldLinks(message);
