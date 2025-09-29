52520675565f6d687cb41af7e34659f0ecb23185
moistbobo <moistbobo@gmail.com>
2025-09-28 21:54:12 -0400

    refactor: change forwarding threshold to 40

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -11,7 +11,9 @@ const Config = {
   DEV_MODE: process.env?.DEV === 'true',
   WORLD_NAME_MATCHERS: process.env.WORLD_NAME_MATCHERS.split(','),
   AUTHOR_NAME_MATCHERS: process.env.AUTHOR_NAME_MATCHERS.split(','),
-  EXPORT_RATE_LIMIT: Number(process.env.EXPORT_RATE_LIMIT) || 1500
+  EXPORT_RATE_LIMIT: Number(process.env.EXPORT_RATE_LIMIT) || 1500,
+  FORWARD_PLAYER_COUNT_THRESHOLD:
+    Number(process.env.FORWARD_PLAYER_COUNT_THRESHOLD) || 40
 };
 
 export default Config;
