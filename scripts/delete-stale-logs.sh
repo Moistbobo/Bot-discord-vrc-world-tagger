7cc15f44efa0d9f6c87ed057152a3931b3b34d36
moistbobo <moistbobo@gmail.com>
2026-07-02 01:33:54 +0800

    chore: add scripts, move docs

--- scripts/delete-stale-logs.sh
+++ scripts/delete-stale-logs.sh
@@ -1,4 +1,6 @@
-LOG_DIR="/dir"
+#!/usr/bin/env bash
+# Delete compressed tslog backups older than 7 days.
+# Intended to run once per week.
+LOG_DIR="/home/world-tagger/release/Bot-discord-vrc-world-tagger"
 
-
-find $BACKUP_DIR -type f -name "*-tslog.log.gz" -mtime +7 -exec rm {} \;
+find "$LOG_DIR" -maxdepth 1 -type f -name "*-tslog.log.gz" -mtime +7 -delete
